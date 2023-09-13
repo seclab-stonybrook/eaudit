9f5757e47e4a0796e2c8e8f314ec28ce9e68e25a
sekar <sekar@cs.stonybrook.edu>
2023-09-13 12:44:58 -0400

    (a) Fixed file permissions, (b) Added error handling in bcc_install.sh, (c) removed a few irrelevant files.

--- bcc_install.sh
+++ bcc_install.sh
@@ -1,17 +1,20 @@
-sudo apt update
+fatal() {
+  echo "BCC installation failed at the following step: $1"
+  exit 1
+}
+
+sudo apt update || fatal "apt update"
 sudo apt install -y zip bison build-essential cmake flex git libedit-dev \
   libllvm14 llvm-14-dev libclang-14-dev python3 zlib1g-dev libelf-dev libfl-dev \
   python3-setuptools \
-  liblzma-dev libdebuginfod-dev arping netperf iperf
-mkdir src
+  liblzma-dev libdebuginfod-dev arping netperf iperf || fatal "apt install (of required development packages)"
+mkdir src || fatal "mkdir"
 cd src
-git clone https://github.com/iovisor/bcc.git
-mkdir bcc/build; cd bcc/build
-cmake ..
-make
-sudo make install
-cmake -DPYTHON_CMD=python3 .. # build python3 binding
+git clone https://github.com/iovisor/bcc.git || fatal "cloning BCC source from iovisor"
+mkdir bcc/build; cd bcc/build 
+cmake .. || fatal "cmake"
+make || fatal "Building BCC from source"
+sudo make install || fatal "installing BCC"
+cmake -DPYTHON_CMD=python3 .. || fatal "building python3 bindings"
 pushd src/python/
-make
-sudo make install
-popd
+( make && sudo make install ) || fatal "installing python bindings"
